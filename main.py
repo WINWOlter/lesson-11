ddd8a147383e0fc8470d3415e07776e1c9396402
WNWolter <113820891+WINWOlter@users.noreply.github.com>
2023-03-26 07:51:38 +0000

    монго

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from bs4 import BeautifulSoup
 URL = "https://porsche.ua/models?category=911"
 print(requests.get(URL).text)
 
+MONGO_URL = "mongodb+srv://markkvu4:<12345>@cluster0.niyzzif.mongodb.net/?retryWrites=true&w=majority"
+
 data = requests.get(URL).text
 
 soup = BeautifulSoup(data, features="html.parser")
