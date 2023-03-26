e227654efcaf3670998369af36dfe42a668d49c2
WNWolter <113820891+WINWOlter@users.noreply.github.com>
2023-03-26 07:47:03 +0000

    виводить ціну  та назву

--- main.py
+++ main.py
@@ -8,4 +8,7 @@ data = requests.get(URL).text
 
 soup = BeautifulSoup(data, features="html.parser")
 
-print(soup.find_all("td"))
+print(soup.find_all("div", {"class":"model-name"}))
+
+print(soup.find_all("span", {"class":"val"}))
+
