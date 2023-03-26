241ccdb992614923128562f3ffcdbe203f436883
WNWolter <113820891+WINWOlter@users.noreply.github.com>
2023-03-26 08:27:24 +0000

    РОБОЧИЙ КОД

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
+from pymongo import MongoClient
 
 URL = "https://porsche.ua/models?category=911"
 print(requests.get(URL).text)
 
-MONGO_URL = "mongodb+srv://markkvu4:<12345>@cluster0.niyzzif.mongodb.net/?retryWrites=true&w=majority"
+MONGO_URL = "mongodb+srv://markkvu4:12345@cluster0.niyzzif.mongodb.net/?retryWrites=true&w=majority"
 
 data = requests.get(URL).text
 
-soup = BeautifulSoup(data, features="html.parser")
+praices = soup = BeautifulSoup(data, features="html.parser")
 
-print(soup.find_all("div", {"class":"model-name"}))
+cars = print(soup.find_all("div", {"class":"model-name"}))
 
 print(soup.find_all("span", {"class":"val"}))
+
+#for f in praices 0:10:
+  #print(f.getText())
+
+#for r in cars 0:10:
+  #print(r.getText())
+
+client = MongoClient(MONGO_URL)
+
+db = client["Kvuch"]
+praices = db["test"]
+praices.insert_one({"praices":"test"})
 
